666126f69cfe09dd4fb9b12cb5e1851aba413adb
monusbence <72020098+monusbence@users.noreply.github.com>
2024-02-22 22:22:37 +0100

    Add files via upload

---
+++ WpfApp3/WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<Versenyzok> versenyzok = Versenyzok.Beolvasas("Selejtezo2012.txt");
+        public MainWindow()
+        {
+            InitializeComponent();
+            versenyzokComboBox.ItemsSource = versenyzok.Select(x => x.Nev);
+            versenyzokComboBox.SelectedItem = "Pars Krisztián";
+
+        }
+        private void versenyzokComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var defaultAlap = versenyzok[versenyzokComboBox.SelectedIndex];
+            CsoportLabel.Content = defaultAlap.Csoport;
+            NemzetLabel.Content = defaultAlap.Nemzet2;
+            NemzetKodLabel.Content = defaultAlap.Kód;
+            SorozatLabel.Content = $"{defaultAlap.D1};{defaultAlap.D2};{defaultAlap.D3}";
+            EredmenyLabel.Content = defaultAlap.Eredmény();
+
+            Uri uri = new Uri("Images/" + defaultAlap.Kód + ".png", UriKind.Relative);
+            flagImage.Source = new BitmapImage(uri);
+        }
+    }
+}
---
+++ WpfApp3/WpfApp3/Versenyzok.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp3
+{
+    internal class Versenyzok
+    {
+        string nev, csoport, nemzet;
+        private double d3;
+        private double eredmeny;
+        private double d1;
+        private double d2;
+
+        public string Nev { get => nev; set => nev = value; }
+        public string Csoport { get => csoport; set => csoport = value; }
+        public string Nemzet { get => nemzet; set => nemzet = value; }
+        public double D1 { get => d1; set => d1 = value; }
+        public double D2 { get => d2; set => d2 = value; }
+        public double D3 { get => d3; set => d3 = value; }
+        public double Eredmeny { get => eredmeny; set => eredmeny = value; }
+        public String Kód => this.Nemzet.Substring(this.Nemzet.IndexOf('(') + 1, 3);
+        public String Nemzet2 => this.Nemzet.Substring(0, this.Nemzet.IndexOf('(') - 1);
+
+        public float Eredmény()
+        {
+            return (float)Math.Max(Math.Max(this.D1, this.D2), this.D3);
+        }
+
+
+        public static ObservableCollection<Versenyzok> Beolvasas(string fajlnev)
+        {
+            ObservableCollection<Versenyzok> Versenyzok = new ObservableCollection<Versenyzok>();
+
+            foreach (var sor in File.ReadLines(fajlnev).Skip(1))
+            {
+                Versenyzok.Add(new Versenyzok(sor));
+            }
+            return Versenyzok;
+        }
+
+
+
+        public Versenyzok(string sor)
+        {
+            string[] adatok = sor.Split(';');
+            this.Nev = adatok[0];
+            this.Csoport = adatok[1];
+            this.Nemzet = adatok[2];
+            D1 = ParseResult(adatok[3]);
+            D2 = ParseResult(adatok[4]);
+            D3 = ParseResult(adatok[5]);
+            this.Eredmeny = Math.Max(D1, Math.Max(D2, D3));
+        }
+
+        private double ParseResult(string eredmeny)
+        {
+            if (eredmeny == "X") return -1.0;
+            if (eredmeny == "-") return -2.0;
+            return double.Parse(eredmeny);
+        }
+    }
+}
